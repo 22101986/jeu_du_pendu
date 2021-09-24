620a771c48396cfc81d4f328edcab542b0dc2bae
harold Pierrache <h.pierrache@gmail.com>
2021-09-24 10:01:00 +0200

    fourthcommit

--- js/jeu_du_pendu.js
+++ js/jeu_du_pendu.js
@@ -4,27 +4,40 @@ let list = [
     ["gorille"],
     ["crapaud"],
     ["baleine"],
-    ["crocodile"],
-    ["kangourou"],
-    ["tarentule"]
+    ["belette"],
+    ["caracal"],
+    ["gazelle"],
+    ["manchot"]
 ];
 
 let score = 7;
 
 let cptChoice = list[Math.floor(Math.random() * list.length)];
 
+let mask = [""]
 
-
-
+let startMessage;
 
 
 // fonctions
 
 
+function start() {
+    startMessage = prompt(`Merci de saisir votre choix`)
 
 
 
+}
 
+
+// logique de code 
+
+alert("Bonjour et bienvenue"+"\\n"
++"pour jouer entrez J"+"\\n"
++"pour quitter entrez Q"+"\\n"
++"pour les règles du jeux entrez R");
+
+start();
 
 
 
