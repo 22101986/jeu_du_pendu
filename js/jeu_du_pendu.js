960c20822cc2671f952c6e0d2ae360957da25d55
harold Pierrache <h.pierrache@gmail.com>
2021-09-26 20:43:19 +0200

    sixteenthcommit

--- js/jeu_du_pendu.js
+++ js/jeu_du_pendu.js
@@ -33,7 +33,7 @@ function start() {
     startMessage = prompt(`Saisir: \n j pour jouer \n r pour les règles du jeu \n q pour quitter`);
     if(startMessage === "j") {
         console.log(cptChoice);
-       return letter = prompt(mask.join(" ") + " Vous avez " + score + " essais, saisir une lettre:");
+       return letter = prompt(`${mask.join(" ")} \nVous avez  ${score} essais, saisir une lettre:`);
     }
     else if(startMessage === "r") {
         alert(`T'es sérieux!!! tu connais pas les règles...`);
@@ -53,17 +53,17 @@ function start() {
 function game(i) {
    if(letter !== cptChoice[i] && score  > 1 ) {
         score -= 1; 
-        letter = prompt(mask.join(" ") + " Vous avez " + score + " essais, saisir une lettre:");
+        letter = prompt(`${mask.join(" ")} \nVous avez  ${score} essais, saisir une lettre:`);
         return game(i);
 }
     if(letter === cptChoice[i] && score > 1) {
         mask[i] = cptChoice[i];
-        letter = prompt(mask.join(" ") + " Vous avez " + score + " essais, saisir une lettre:");
+        letter = prompt(`${mask.join(" ")} \nVous avez  ${score} essais, saisir une lettre:`);
         return game(i);
 
     }
     if(score === 1) {
-        alert("Dommage vous avez perdu...");
+        alert(`Dommage vous avez perdu...\n Le mot était ${cptChoice.join("")}.`);
     }
     
     
